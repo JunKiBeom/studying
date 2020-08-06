c9619ba480b05791974f2fb2e7ec38bf96c6ed67
JunKiBeom <dona0408@gmail.com>
2020-08-06 19:36:11 +0900

    first swift

---
+++ AxB.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_Commd
+//
+//  Created by 전기범 on 2020/08/06.
+//  Copyright © 2020 전기범. All rights reserved.
+//
+
+import Foundation
+
+let a = readLine()?.split(separator: " ").map{Int($0)!}
+
+var sol = Int(a![0]) * Int(a![1])
+
+print(sol)
