7ebf209bdf98fa1e4efb4e2c829b8ec5d25f79d2
JunKiBeom <dona0408@gmail.com>
2020-11-08 12:32:13 +0900

    navigator

---
+++ LayoutProps/routes/MainStackNavigator.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Layout from "../screens/Layout"
+
+const Stack = createStackNavigator();
+
+function MainStackNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator 
+                initialRouteName='Layout'
+            >
+                <Stack.Screen 
+                    name = "Layout"
+                    component = {Layout}
+                    options={{ title: 'layout Screen'}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default MainStackNavigator
