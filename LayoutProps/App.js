6e6267d2465c325c5afdef44b82b0c179bebf78d
JunKiBeom <dona0408@gmail.com>
2020-11-08 12:24:40 +0900

    Layout

--- LayoutProps/App.js
+++ LayoutProps/App.js
@@ -1,21 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'; //자바스크립트 버전에 따라 리액트 라이브러리에 진입
+import {Button, View, StyleSheet, ScrollView} from 'react-native'; //리액트 네이티브 사용
+import Constants from 'expo-constants';
 
-export default function App() {
+const Layout = () => {
+
+  const flexDirections = ['row','row-reverse','column','column-reverse'];
+  const justifyContents = [
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around',
+    'space-evenly',
+  ];
+  const alignItemsArr = [
+    'flex-start',
+    'flex-end',
+    'center',
+    'stretch',
+    'baseline',
+  ];
+  const wraps = ['nowrap','wrap','wrap-reverse'];
+  const directions = ['inherit','ltr','rtl'];
+
+  const [flexDirection, setFlexDirection] = useState(0);
+  const [justifyContent, setjustifyContent] = useState(0);
+  const [alignItems, setAlignItems] = useState(0);
+  const [direction, setdirection] = useState(0);
+  const [wrap, setwrap] = useState(0);
+
+const hookedStyles = {
+    flexDirection: flexDirections[flexDirection],
+    justifyContent: justifyContents[justifyContent],
+    alignItems: alignItemsArr[alignItems],
+    direction: directions[direction],
+    flexWrap: wraps[wrap],
+  };
+
+  const changeSetting = (value, options, setterFunction) => {
+    if (value == options.length - 1) {
+      setterFunction(0);
+    } else {
+    setterFunction(value + 1);
+    }
+    console.log(options[value]);
+  };
+
+  const Square = () => {
+    const sqStyle = {
+      width: 50,
+      height: 50,
+      backgroundColor: randomHexColor(),
+    };
+    return <View style={sqStyle} />;
+  };
+
+  const [squares, setSquares] = useState([Square(), Square(), Square()]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={{ paddingTop: Constants.statusBarHeight}} />
+      <View style={[styles.container, styles.playingSpacem, hookedStyles]}>
+        {squares.map(elem => elem)}
+        
+      </View>
+      <ScrollView style={[styles.container]}>
+        <View style={[styles.controlSpace]}>
+          <View style={styles.buttonView}>            
+            <Button title="CHANGE FLEX DIRECTION"
+              onPress={() =>
+                changeSetting(flexDirection, flexDirections, setFlexDirection)
+            }
+            />
+          </View>
+          
+          <View style={styles.buttonView}>            
+            <Button title="CHANGE JUSTIFY CONTENT"
+            onPress={() => changeSetting(justifyContent,justifyContents,setjustifyContent)
+          }
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="CHANGE ALIGN ITEMS"
+            onPress={() => changeSetting(alignItems, alignItemsArr, setAlignItems)
+            }
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="CHANGE DIRECTION"
+            onPress={() => 
+              changeSetting(direction, directions, setdirection)
+            }
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="CHANGE FLEX WRAP"
+            onPress={() => 
+              changeSetting(wrap, wraps, setwrap)
+            }
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="ADD SQUARE"
+            onPress={() => 
+              setSquares([...squares, Square()])
+            }
+            />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="DELETE SQUARE"
+            onPress={() => 
+              setSquares(squares.filter((v, i) => i != squares.length-1)) /// 뒤에서 부터 지우기
+              // i != 0 은 앞에서 부터 지우기
+            }
+            />
+          </View>
+        </View>
+
+      </ScrollView>
+
+    </>
+
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '50%',
   },
-});
+  playingSpace: {
+    backgroundColor: 'white',
+    borderColor: 'blue',
+    borderWidth: 3,
+  },
+  controlSpace: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#F5F5F5',
+
+  },
+  buttonView: {
+    width: '50%',
+    padding: 10,
+  }
+})
+
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, () => {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
+
+export default Layout;
