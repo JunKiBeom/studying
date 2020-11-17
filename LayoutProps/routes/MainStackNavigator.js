57c655653d0bce68dfc09b06597459e2488eb4b4
JunKiBeom <dona0408@gmail.com>
2020-11-17 20:16:03 +0900

    react-navigator

--- LayoutProps/routes/MainStackNavigator.js
+++ LayoutProps/routes/MainStackNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Layout from "../screens/Layout"
+import Home from "../screens/home"
 
 const Stack = createStackNavigator();
 
@@ -10,13 +11,10 @@ function MainStackNavigator() {
     return (
         <NavigationContainer>
             <Stack.Navigator 
-                initialRouteName='Layout'
+                initialRouteName='Home'
             >
-                <Stack.Screen 
-                    name = "Layout"
-                    component = {Layout}
-                    options={{ title: 'layout Screen'}}
-                />
+                <Stack.Screen name = "Home" component={Home} options = {{title : 'home screen'}}/>
+                <Stack.Screen name = "Layout" component = {Layout} options={{ title: 'layout Screen'}}/>
             </Stack.Navigator>
         </NavigationContainer>
 
